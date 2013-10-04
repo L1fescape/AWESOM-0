55d5dbf9b48be51cb32ab903008c00dc5ef547f4
Andrew Crites <acrites@mobiquityinc.com>
2013-10-04 13:25:04 -0400

    RPS: improving nick check/AWESOM-0 responds

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,14 +1,15 @@
 module.exports = function (bot) {
-  bot.respond(/^rps (\w+)/, "rps - Play rock/paper/scissors", function (msg) {
-      bot.client.whois(msg.match[1], function (user) {
+  bot.respond(/^rps ([\w-]+)/, "rps - Play rock/paper/scissors", function (msg) {
+      var opponent = msg.match[1];
+      bot.client.whois(opponent, function (user) {
           if (!user.user) {
-              bot.client.say(msg.channel, msg.match[1] + " is not online!");
+              bot.client.say(msg.channel, opponent + " is not online!");
           }
           else if (user.nick === msg.from) {
               bot.client.say(msg.channel, "You can't play with yourself!");
           }
-          else {
-              
+          else if (bot.client.nick === opponent) {
+              bot.client.say(msg.channel, "I suppose I will play with you");
           }
       });
   });
