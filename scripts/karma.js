eb8167bcbaeddf648d9592a5ff2dbe31f5d2abf6
Marco Munizaga <marco.munizaga@ufl.edu>
2013-11-01 15:53:00 -0400

    Update karma.js

--- scripts/karma.js
+++ scripts/karma.js
@@ -1,7 +1,7 @@
 var karma = {}
 
 module.exports = function(bot) {
-  bot.respond(/\w+\+\+$|\w+--$/i, "Keep track of people's karma", function(msg) {
+  bot.respond(/\w+\+\+$|\w+--$/i, "<user>++ or <user>-- : Record karma", function(msg) {
     var doInc =  msg.message.match(/\w+\+\+$/) !== null;
     var person = msg.message.match(/\w+\+\+$|\w+--/)[0].replace(/\+\+$|--$/,"")
     var oldKarma = karma[person] === undefined ? 0 : karma[person]
