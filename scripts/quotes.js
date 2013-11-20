b65840f6ecc3e2bd1eb6d0590c09821349e6893e
Andrew Kennedy <akennedy2292@gmail.com>
2013-11-20 14:47:18 -0500

    updated quotes script to allow multiple quotes to be saved

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,19 +1,58 @@
 var quotes = {}
 
 module.exports = function(bot) {
-  bot.respond(/^(remember|quote) \w+/i, "quote <user> [<quote>] - remember/retrieve quotes", function(msg) {
-    var response
-    var text = msg.message.replace(/^(remember|quote)\ /,"").split(/\ /g)
-    var key = text[0]
-    //either store or recall depending on the amount of spaces i.e. "quote akenn" means to retrieve
-    //while "quote akenn lol babes are k3wl" means to store a message
-    if (text.length > 1){
-      //store message
-      var rest = text.slice(1).join(" ")
-      quotes[key]=rest
-      response = "quoting "+key+" with "+rest
-    }else{
-      response = key+": "+quotes[key]
+
+
+  bot.respond(/^quotes/i, "quotes [<user>] - retrieve all quotes or a specific user's quotes", function(msg) {
+    var response = "";
+    var user = msg.message.split(" ")[1];
+   
+    if (user) {
+      // if there are no quotes for a user
+      if (typeof quotes[user] === 'undefined' || quotes[user].length == 0)
+        resonse = "There are no quotes for this user.";
+
+      // else print all that user's quotes
+      else {
+        for (var i = 0, j = quotes[user].length; i < j; i++) {
+          response += quotes[user][i] + "\n"; 
+        }
+      }
+    }
+
+    // else get all quotes for all users
+    else {
+      for (var user in quotes) {
+        if (quotes[user].length) {
+          response += user + "'s quotes: \n";
+          for (var i = 0, j = quotes[user].length; i < j; i++) {
+            response += quotes[user][i] + "\n";
+          }
+        }
+      }
+    } 
+    bot.client.say(msg.channel, response);
+      
+  });
+      
+
+  bot.respond(/^(remember|quote) \w+/i, "quote <user> <quote> - remember/retrieve quotes", function(msg) {
+    var response = "";
+    var text = msg.message.replace(/^(remember|quote)\ /,"").split(" ");
+
+    if (text.length > 1) {
+      var quote = text.splice(1).join(" ");
+      var user = text;
+
+      if (typeof quotes[user] === 'undefined')
+        quotes[user] = [];
+
+      quotes[user].push(quote);
+
+      response = "quoting " + user + " with " + quote
+    }
+    else {
+      response = "Please include a quote."
     }
 
     bot.client.say(msg.channel, response);
