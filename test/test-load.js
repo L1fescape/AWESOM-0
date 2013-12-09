deb1cdc73742e8459092e7f03d5e2d740aea5aa8
Andrew Kennedy <akennedy2292@gmail.com>
2013-12-08 19:23:20 -0500

    started writing tests

---
+++ test/test-load.js
@@ -0,0 +1,8 @@
+var assert  = require('assert');
+
+describe('AWESOM0', function () {
+  it('can be imported without blowing up', function () {
+    var awesom0 = require('../awesom0');
+    assert(awesom0 !== undefined);
+  });
+});
