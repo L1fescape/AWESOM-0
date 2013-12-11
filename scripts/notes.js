f9194e66d29a851b205fd9db2689edbf6b752858
Andrew Kennedy <akennedy2292@gmail.com>
2013-12-11 14:41:13 -0500

    store notes with redis

--- scripts/notes.js
+++ scripts/notes.js
@@ -1,61 +1,100 @@
-var moment = require('moment');
+var moment = require('moment'),
+    redis = require("redis"),
+    client = redis.createClient();
 
-var notes = {};
-var seenNotes = {};
 
 module.exports = function(bot) {
 
-  bot.userJoin(function(msg) {
-    var user = msg.nick.toLowerCase();
-    if (seenNotes[user] === false) {
-      bot.client.say(msg.channel, msg.nick + ", you have notes!");
-      seenNotes[user] = true;
-    }
-  });
+  function addNote(user, note, callback) {
+    getNotes(user, function(notes) {
+      notes.notes.push(note);
+      notes.seen = false;
+      setNotes(user, notes);
+      callback();
+    });
+  }
 
+  function getNotes(user, callback) {
+    client.get(user, function(err, notes) {
+      try {
+        notes = JSON.parse(notes);
+      }
+      catch (err) {
+        notes = {
+          seen: true,
+          notes: []
+        };
+      }
+      if (notes === null)
+        notes = {
+          seen: true,
+          notes: []
+        };
+      setNotes(user, notes);
 
-  bot.hear(/.*/i, function(msg) {
-    var user = msg.from.toLowerCase();
-    if (seenNotes[user] === false) {
-      bot.client.say(msg.channel, msg.from + ", you have notes!");
-      seenNotes[user] = true;
-    }
-  });
+      callback(notes);
+    });
+  }
+
+  function setNotes(user, notes) {
+    notes = JSON.stringify(notes)
+    client.set(user, notes, redis.print);
+  }
+
+  function checkSeenNotes(msg) {
+    var user = (typeof msg.from !== 'undefined') ? msg.from.toLowerCase() : msg.nick.toLowerCase();
+    getNotes(user, function(notes) {
+      if (notes.seen === false) {
+        bot.client.say(msg.channel, user + ", you have notes!");
+        notes.seen = true;
+      }
+
+      setNotes(user, notes);
+    });
+  }
+
+  bot.userJoin(checkSeenNotes);
+
+  bot.hear(/.*/i, checkSeenNotes);
 
 
   bot.respond(/^notes$/i, "notes - Display notes left for you", function(msg) {
     var response = "";
     var user = msg.from.toLowerCase();
-    if (notes[user] && notes[user].length) {
-      for (var i = 0, note; note = notes[user][i]; i++) {
+    getNotes(user, function(userNotes) {
+      for (var i = 0; i < userNotes.notes.length; i++) {
+        var note = userNotes.notes[i];
         response += note.from + " @ " + moment(note.time).fromNow() + ": " + note.msg + "\n";
       }
-      notes[user] = [];
-    }
-    else 
-      response = "No new notes.";
+      userNotes.notes = [];
+      
+      if (!response)
+        response = "No new notes.";
+      
+      userNotes.seen = true;
 
-    bot.client.say(msg.channel, response);
+      setNotes(user, userNotes);
+
+      bot.client.say(msg.channel, response);
+    });
   });
 
 
   bot.respond(/^note (.*)?/i, "note <user> <message> - Leave a note for a user", function(msg) {
     var tokens = msg.match[1].split(" ");
     var user = tokens.splice(0, 1).toString().toLowerCase();
-    if (typeof notes[user] === 'undefined')
-      notes[user] = [];
-    notes[user].push({from: msg.from, time: new Date(), msg: tokens.join(" ")});
-    seenNotes[user] = false; 
-    var confirmations = [
-      "Okie dokes!",
-      "Done.",
-      "Saved.",
-      "Got it!"
-    ];
-    response = confirmations[Math.floor(Math.random()*confirmations.length)];
-    bot.client.say(msg.channel, response);
+    var note = {from: msg.from, time: new Date(), msg: tokens.join(" ")};
+
+    addNote(user, note, function() {
+      var confirmations = [
+        "Okie dokes!",
+        "Done.",
+        "Saved.",
+        "Got it!"
+      ];
+      response = confirmations[Math.floor(Math.random()*confirmations.length)];
+      bot.client.say(msg.channel, response);
+    });
   });
 
-
-}
-
+};
