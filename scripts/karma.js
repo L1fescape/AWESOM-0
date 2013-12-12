e56045eb96972abe85a88a6b5f8020cc2fb3630c
Andrew Kennedy <akennedy2292@gmail.com>
2013-12-12 09:34:30 -0500

    added ability to check karma. prevent user from giving self karma

--- scripts/karma.js
+++ scripts/karma.js
@@ -1,19 +1,39 @@
 module.exports = function(bot) {
   bot.hear(/\w+\+\+$|\w+--$/i, "<user>++ or <user>-- : Record karma", function(msg) {
     var person = msg.message.match(/\w+\+\+$|\w+--/)[0].replace(/\+\+$|--$/,"")
+    
+    if (person != msg.from) {
 
-    bot.db.get("karma " + person, function(karma) {
+      bot.db.get("karma " + person, function(karma) {
+        if (!karma)
+          karma = 0;
+        if (/\w+\+\+$/.test(msg.message))
+          ++karma
+        else
+          --karma
+        bot.db.set("karma " + person, karma);
+
+        var response = person + " ⇒ " + karma;
+        bot.client.say(msg.channel, response);
+      });
+    }
+
+    else {
+      bot.client.say(msg.channel, "Sorry, you can't give yourself karma.");
+    }
+
+  });
+
+
+
+  bot.respond(/^karma$/i, "karma - retreive karma level", function(msg) {
+
+    bot.db.get("karma " + msg.from, function(karma) {
       if (!karma)
         karma = 0;
-      if (/\w+\+\+$/.test(msg.message))
-        ++karma
-      else
-        --karma
-      bot.db.set("karma " + person, karma);
 
-      var response = person + " ⇒ " + karma;
+      var response = msg.from + " ⇒ " + karma;
       bot.client.say(msg.channel, response);
     });
-
   });
 };
