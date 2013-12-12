9023eda1cc1959ee2e89259d9e220bec9412a334
Andrew Kennedy <akennedy2292@gmail.com>
2013-12-11 22:26:36 -0500

    removed sprintf dependency

--- scripts/imageme.js
+++ scripts/imageme.js
@@ -1,23 +1,17 @@
-var 
-  // libraries
-  vsprint = require("sprintf-js").vsprintf, 
-  request = require('request');
+var request = require('request');
 
 
 module.exports = function(bot) {
 
   
   bot.respond(/(image me|imageme) (.*)/i, "image me <term> - Search google for images of <term>", function(msg) {
-    // google image search base url
-    var base_url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&start=%s'
-    // the best search term ever
+    var url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q='
     var query = msg.match[2]
-    // google paginates their results, so let's pick a random result page to pull a pug from
     var start = Math.round(Math.random()*20)
-    // sub in our query and pagination start and make the request 
-    var url = vsprint(base_url, [query, start])
+    url += query + "&start=" + start
+
     // make the request
-    request((url), function (error, response, body) {
+    request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         // grab the actual search results out of the response
         var results = JSON.parse(body)['responseData']['results'];
